3a2d2665ea3275b13826f37fb16f578968df4cf8
Алёна Остапенко <lexa.trukry319@gmail.com>
2025-11-25 23:26:15 +0300

    игры разума

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig({
+  files: ['**/*.{js,mjs,cjs}'],
+  languageOptions: {
+    globals: globals.browser,
+  },
+  plugins: {
+    '@stylistic': stylistic,
+  },
+  rules: {
+
+    '@stylistic/semi': ['error', 'never'],
+    '@stylistic/eol-last': ['error', 'always'],
+    '@stylistic/no-multiple-empty-lines': ['error', { max: 1 }],
+    '@stylistic/quotes': ['error', 'single'],
+    '@stylistic/no-tabs': 'error',
+    '@stylistic/indent': ['error', 2],
+    '@stylistic/object-curly-spacing': ['error', 'always'],
+    '@stylistic/no-trailing-spaces': 'error',
+    '@stylistic/brace-style': ['error', '1tbs'],
+    '@stylistic/padded-blocks': ['error', 'never'],
+
+    // Базовые правила ESLint
+    'no-unused-vars': 'warn',
+    'no-undef': 'error',
+  },
+})
