975d2464d43091bfecf2f0849fddeb3236216a7a
Алёна Остапенко <lexa.trukry319@gmail.com>
2026-01-16 17:25:27 +0300

    Игры разума

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import stylistic from '@stylistic/eslint-plugin'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig({
   files: ['**/*.{js,mjs,cjs}'],
