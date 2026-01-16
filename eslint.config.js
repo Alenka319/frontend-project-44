a91a1c91cab5eadd59c929fed8711efce876eb8e
Алёна Остапенко <lexa.trukry319@gmail.com>
2026-01-16 18:33:24 +0300

    Игры разума 3

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,31 @@
 import stylistic from '@stylistic/eslint-plugin'
 import globals from 'globals'
-import { defineConfig } from 'eslint/config'
 
-export default defineConfig({
-  files: ['**/*.{js,mjs,cjs}'],
-  languageOptions: {
-    globals: globals.browser,
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      '@stylistic/semi': ['error', 'never'],
+      '@stylistic/eol-last': ['error', 'always'],
+      '@stylistic/no-multiple-empty-lines': ['error', { max: 1 }],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/no-tabs': 'error',
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/object-curly-spacing': ['error', 'always'],
+      '@stylistic/no-trailing-spaces': 'error',
+      '@stylistic/brace-style': ['error', '1tbs'],
+      '@stylistic/padded-blocks': ['error', 'never'],
+      'no-unused-vars': 'warn',
+      'no-undef': 'error',
+    },
   },
-  plugins: {
-    '@stylistic': stylistic,
-  },
-  rules: {
-
-    '@stylistic/semi': ['error', 'never'],
-    '@stylistic/eol-last': ['error', 'always'],
-    '@stylistic/no-multiple-empty-lines': ['error', { max: 1 }],
-    '@stylistic/quotes': ['error', 'single'],
-    '@stylistic/no-tabs': 'error',
-    '@stylistic/indent': ['error', 2],
-    '@stylistic/object-curly-spacing': ['error', 'always'],
-    '@stylistic/no-trailing-spaces': 'error',
-    '@stylistic/brace-style': ['error', '1tbs'],
-    '@stylistic/padded-blocks': ['error', 'never'],
-
-    // Базовые правила ESLint
-    'no-unused-vars': 'warn',
-    'no-undef': 'error',
-  },
-})
+]
